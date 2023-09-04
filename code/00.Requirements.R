7dc01395e4b8a3a1b54066ce15ab6cbc29ce4d15
Zhenyu Liu <37285516+liuzhenyu-yyy@users.noreply.github.com>
2023-09-04 17:44:22 +0800

    move function to 00.Requirements.R

--- code/00.Requirements.R
+++ code/00.Requirements.R
@@ -27,3 +27,37 @@ if (TRUE) {
 
 project.dir.all <- "E:/LabWork/Project/CRC_NGS_ATAC/CRC_Epi_scATAC/Project_Dir_All"
 project.dir.epi <- "E:/LabWork/Project/CRC_NGS_ATAC/CRC_Epi_scATAC/Project_Dir_Epi"
+
+homer.parser <- function(res, log.p.value = 50, log2.enrichment = 1) {
+    res <- fread(res,
+        header = TRUE
+    ) %>%
+        as.data.frame() %>%
+        .[order(.[, 1]), ]
+    colnames(res) <- c(
+        "Name", "Consensus", "p.value", "log.p.value", "q.value",
+        "n.Targets", "Perc.Targets", "n.Background", "Perc.Background"
+    )
+    res$Perc.Targets <- res$Perc.Targets %>%
+        gsub("%", "", .) %>%
+        as.numeric()
+    res$Perc.Background <- res$Perc.Background %>%
+        gsub("%", "", .) %>%
+        as.numeric()
+    res$Log2_Enrichment <- log2(res$Perc.Targets / res$Perc.Background)
+    res$log.p.value <- 0 - res$log.p.value
+    res$TF <- gsub("\\(.+?$", "", res$Name) %>% toupper()
+    res <- res[!duplicated(res$TF), ]
+    rownames(res) <- res$TF
+
+    anno <- read.csv("homer/motif.anno.csv", header = TRUE)
+    rownames(anno) <- anno$TF
+    res$Family <- anno[res$TF, "Anno"]
+
+    # res <- res[order(res$log.p.value, decreasing = TRUE), ]
+    # res <- res[!duplicated(res$Family), ]
+
+    res$Diff <- "none"
+    res$Diff[res$Log2_Enrichment >= log2.enrichment & res$log.p.value >= log.p.value] <- "up"
+    return(res)
+}
