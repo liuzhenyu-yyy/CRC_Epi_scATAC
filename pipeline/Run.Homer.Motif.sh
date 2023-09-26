33c4db5b5017a3dcdd619ba8da9881c20a20a3bf
Zhenyu Liu <37285516+liuzhenyu-yyy@users.noreply.github.com>
2023-09-26 11:46:46 +0800

    add homer script

---
+++ pipeline/Run.Homer.Motif.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+findMotifsGenome=/mnt/d/WSL2/homer/bin/findMotifsGenome.pl
+bed_dir=/mnt/e/LabWork/Project/CRC_NGS_ATAC/CRC_Epi_scATAC/Results/02.Epi_Molecular_Subtype/bed/cluster
+
+ls ${bed_dir} |
+    awk '{gsub(".bed", "");gsub("marker.peak.tumor.", ""); print $1}' > sample.list
+split -l 4 sample.list -d -a 1  sample.list
+
+for i in {0..6}
+do  
+    echo -e "["$(date)"]\tStart job submission round ${i}...." 
+
+    while read sample; do
+        nohup ${findMotifsGenome} \
+            ${bed_dir}/marker.peak.tumor.${sample}.bed \
+            hg38 homer/clusters/${sample} \
+            -size 200 \
+            > homer/homer.${sample}.log 2>&1 &
+
+        echo -e "["$(date)"]\tSubmit job for ${sample}...."
+done < sample.list${i};
+sleep 60m
+done
+
