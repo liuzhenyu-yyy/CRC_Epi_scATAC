d584364c38f06a2e932c46ceb10e5ecb812abb0a
Zhenyu Liu <37285516+liuzhenyu-yyy@users.noreply.github.com>
2024-01-20 14:37:17 +0800

    NV 10X CRC atlas

--- code/Integration/scRNA_10X_CRC_atlas.R
+++ code/Integration/scRNA_10X_CRC_atlas.R
@@ -1,8 +1,15 @@
-setwd("E:/LabWork/Project/CRC_NGS_ATAC/CRC_Epi_scATAC/Results/07.Independent_Validation")
+#####################################################
+#
+# Re-analysis of Nature Genetics CRC 10X atlas
+# Data source: NCBI Gene Expression Omnibus GSE132465
+# Citation: https://www.nature.com/articles/s41588-020-0636-z
+#
+#####################################################
 
+setwd("E:/LabWork/Project/CRC_NGS_ATAC/CRC_Epi_scATAC/Results/07.Independent_Validation")
+load("Independent_Validation.RNA.RData")
 source("../../code/00.Requirements.R")
 proj_Epi <- loadArchRProject(project.dir.epi, force = TRUE)
-
 
 # 1. Primary analysis of CRC 10X atlas -----
 dir.create("NG_10X")
@@ -140,7 +147,7 @@ DimPlot(epi.obj, group.by = "Patient") +
     coord_fixed()
 dev.off()
 
-## 2.1. Malignant vs Normal ----
+## 2.2. Malignant vs Normal ----
 table(epi.obj$Cell_subtype, epi.obj$Class)
 marker.10X <- FindMarkers(
   epi.obj,
@@ -150,10 +157,190 @@ marker.10X <- FindMarkers(
   min.pct = 0.05,
   logfc.threshold = 0.01,
 )
+saveRDS(marker.10X, "NG_10X/marker.10X.RDS")
+marker.10X$Gene <- rownames(marker.10X)
+
+marker.10X$DEG <- "none"
+marker.10X$DEG[marker.10X$avg_log2FC > 0.25 & marker.10X$p_val_adj < 0.05] <- "up"
+marker.10X$DEG[marker.10X$avg_log2FC < -0.25 & marker.10X$p_val_adj < 0.05] <- "down"
+table(marker.10X$DEG)
+
+pdf("NG_10X/Volcano.10X.pdf", 3.5, 3)
+ggplot(marker.10X, aes(x = avg_log2FC, y = -log10(p_val_adj), color = DEG)) +
+    geom_point(size = 0.1) +
+    geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
+    geom_vline(xintercept = c(-0.2, 0.2), linetype = "dashed") +
+    # ylim(c(0, 200)) +
+    xlim(c(-4, 4)) +
+    scale_color_manual(values = c("red", "gray", "blue")) +
+    theme_classic() +
+    xlab("Log2 fold change") +
+    ylab("-log10 adjusted p-value")
+dev.off()
+
+marker.10X.list <- list(
+    up = marker.10X$Gene[marker.10X$DEG == "up"],
+    down = marker.10X$Gene[marker.10X$DEG == "down"]
+)
+
+table(epi.obj$Class, epi.obj$Patient)
+
+## 2.3. marker patient ----
+epi.obj$iCMS <- "none"
+epi.obj$iCMS[grep("CMS1", epi.obj$Cell_subtype)] <- "iCMS3"
+epi.obj$iCMS[grep("CMS2", epi.obj$Cell_subtype)] <- "iCMS2"
+epi.obj$iCMS[grep("CMS3", epi.obj$Cell_subtype)] <- "iCMS3"
+epi.obj$iCMS[grep("CMS", epi.obj$Cell_subtype, invert = TRUE)] <- "Normal"
+table(epi.obj$iCMS)
+
+epi.obj$Patient_Tumor <- epi.obj$Patient
+epi.obj$Patient_Tumor[epi.obj$Class == "Normal"] <- "Normal"
+table(epi.obj$Patient_Tumor)
+
+patients <- unique(epi.obj$Patient_Tumor) %>% setdiff("Normal")
+
+marker.10x.patient.up <- list()
+marker.10x.patient.down <- list()
+
+for (one in patients) {
+    message(Sys.time(), ": Find markers for ", one, "...")
+    temp <- FindMarkers(
+        epi.obj,
+        group.by = "Patient_Tumor",
+        ident.1 = one,
+        ident.2 = "Normal"
+    )
+    marker.10x.patient.up[[one]] <- rownames(temp)[temp$avg_log2FC > 0.25 & temp$p_val_adj < 0.05]
+    marker.10x.patient.down[[one]] <- rownames(temp)[temp$avg_log2FC < (-0.25) & temp$p_val_adj < 0.05]
+}
+
+length(marker.10x.patient.up %>% unlist() %>% unique())
+table(marker.10X$DEG)
+
+temp <- table(epi.obj$Patient_Tumor, epi.obj$iCMS)
+temp2 <- colnames(temp)[apply(temp, 1, function(x) {
+    which.max(x)
+})]
+names(temp2) <- rownames(temp)
+temp2 <- sort(temp2)
+temp2 <- temp2[c(24, 1:23)]
+
+# patient heatmap
+gene.selected <- c(
+    unlist(marker.10x.patient.up[names(temp2[2:24])]),
+    unlist(marker.10x.patient.down[names(temp2[2:24])])
+) %>% unique()
+length(unlist(marker.10x.patient.up) %>% unique())
+length(unlist(marker.10x.patient.down) %>% unique())
+# gene.selected <- gene.selected[order(marker.10X[gene.selected, "avg_log2FC"], decreasing = TRUE)]
+
+plot.data <- AverageExpression(epi.obj,
+    features = gene.selected,
+    group.by = "Patient_Tumor"
+)
+plot.data <- plot.data$RNA %>% as.data.frame()
+identical(rownames(plot.data), gene.selected)
+
+anno.row <- data.frame(
+    row.names = gene.selected,
+    "Gene" = ifelse(gene.selected %in% unlist(marker.10x.patient.up), "up", "down")
+)
+table(anno.row$Gene)
+
+anno.col <- data.frame(
+    row.names = names(temp2),
+    "iCMS" = temp2
+)
+
+plot.data <- plot.data[rownames(anno.row), rownames(anno.col)]
+plot.data.nor <- apply(plot.data, 1, function(x) {
+    x <- (x - mean(x)) / sd(x)
+}) %>% t()
+plot.data.nor[plot.data.nor > 2] <- 2
+plot.data.nor[plot.data.nor < -2] <- -2
+png("NG_10X/Heatmap.DEG.patient.png", 600, 600)
+pheatmap::pheatmap(
+    plot.data.nor,
+    annotation_col = anno.col,
+    annotation_row = anno.row,
+    cluster_rows = FALSE,
+    cluster_cols = FALSE,
+    show_rownames = FALSE,
+    show_colnames = TRUE,
+    color = colorRampPalette(c("blue", "white", "red"))(100),
+    annotation_colors = list(
+        iCMS = c("Normal" = "#208a42", "iCMS2" = "#283891", "iCMS3" = "#62b7e6"),
+        Gene = c("up" = "#cd2525", "down" = "#1774cd")
+    )
+)
+dev.off()
+
+## 2.4. known iCMS signature ----
+markers.iCMS <- read.table("E:/LabWork/Project/CRC_NGS_ATAC/iCMS markers.txt",
+    stringsAsFactors = FALSE,
+    sep = "\t", header = TRUE
+)
+markers.iCMS <- base::as.list(markers.iCMS) %>% sapply(function(x) setdiff(x, ""))
+lapply(markers.iCMS, length)
+(308 + 74) / 5160 # 7.4%
+(279 + 54) / 1468 # 22.7%
+
+markers.iCMS <- lapply(markers.iCMS, function(x) intersect(x, rownames(epi.obj)))
+lapply(markers.iCMS, length)
+
+gene.selected <- unlist(markers.iCMS)
+
+plot.data <- AverageExpression(epi.obj,
+    features = gene.selected,
+    group.by = "Patient_Tumor"
+)
+plot.data <- plot.data$RNA %>% as.data.frame()
+
+anno.row <- data.frame(
+    row.names = gene.selected,
+    "Gene" = names(gene.selected) %>%
+        gsub("[0-9]$", "", .) %>%
+        gsub("[0-9]$", "", .) %>%
+        gsub("[0-9]$", "", .)
+)
+table(anno.row$Gene)
+
+anno.col <- data.frame(
+    row.names = names(temp2),
+    "iCMS" = temp2
+)
+
+plot.data <- plot.data[rownames(anno.row), rownames(anno.col)]
+plot.data.nor <- apply(plot.data, 1, function(x) {
+    x <- (x - mean(x)) / sd(x)
+}) %>% t()
+plot.data.nor[plot.data.nor > 2] <- 2
+plot.data.nor[plot.data.nor < -2] <- -2
+png("NG_10X/Heatmap.iCMS.geme.patient.png", 600, 600)
+pheatmap::pheatmap(
+    plot.data.nor,
+    annotation_col = anno.col,
+    annotation_row = anno.row,
+    cluster_rows = FALSE,
+    cluster_cols = FALSE,
+    show_rownames = FALSE,
+    show_colnames = TRUE,
+    color = colorRampPalette(c("blue", "white", "red"))(100),
+    annotation_colors = list(
+        iCMS = c("Normal" = "#208a42", "iCMS2" = "#283891", "iCMS3" = "#62b7e6"),
+        Gene = c("iCMS2_Up" = "#c80505", "iCMS2_Down" = "#064e91",  "iCMS3_Up" = "#DC7C7C", "iCMS3_Down" = "#66aced")
+    ),
+    gaps_row = sum(length(markers.iCMS$iCMS2_Up), length(markers.iCMS$iCMS2_Down)),
+    gaps_col = c(1, 15)
+)
+dev.off()
+
+length(markers.iCMS$iCMS2_Up) / length(unlist(marker.10x.patient.up) %>% unique())
+rm(plot.data, plot.data.nor, anno.row, anno.col, patients, one, temp, temp2)
 
 # 3. Diff gene ----
 ## 3.1. TCGA RNA data ----
-marker.TCGA <- read.table("TCGA/TCGA.GEPIA.Limma.txt", sep = "\t", header = TRUE)
+marker.TCGA <- read.table("data/TCGA/TCGA.GEPIA.Limma.txt", sep = "\t", header = TRUE)
 colnames(marker.TCGA) <- c("gene", "gene_id", "mean_tumor", "mean_normal", "log2fc", "p_adj")
 marker.TCGA <- marker.TCGA[!duplicated(marker.TCGA$gene), ]
 rownames(marker.TCGA) <- marker.TCGA$gene
@@ -180,40 +367,7 @@ marker.TCGA.list <- list(
     down = marker.TCGA$gene[marker.TCGA$DEG == "down"]
 )
 
-## 3.2. 10X RNA-seq data ----
-marker.10X <- readRDS("10X/marker.10X.RDS")
-marker.10X$DEG <- "none"
-marker.10X$DEG[marker.10X$avg_log2FC > 0.25 & marker.10X$p_val_adj < 0.05] <- "up"
-marker.10X$DEG[marker.10X$avg_log2FC < -0.25 & marker.10X$p_val_adj < 0.05] <- "down"
-table(marker.10X$DEG)
-
-pdf("Volcano.10X.pdf", 3.5, 3)
-ggplot(marker.10X, aes(x = avg_log2FC, y = -log10(p_val_adj), color = DEG)) +
-    geom_point(size = 0.1) +
-    geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
-    geom_vline(xintercept = c(-0.2, 0.2), linetype = "dashed") +
-    # ylim(c(0, 200)) +
-    xlim(c(-4, 4)) +
-    scale_color_manual(values = c("red", "gray", "blue")) +
-    theme_classic() +
-    xlab("Log2 fold change") +
-    ylab("-log10 adjusted p-value")
-dev.off()
-
-marker.10X.list <- list(
-    up = marker.10X$Gene[marker.10X$DEG == "up"],
-    down = marker.10X$Gene[marker.10X$DEG == "down"]
-)
-
-## 3.3. ATAC data ----
-DEG.ATAC <- read.table("DEG.ATAC.txt", sep = "\t", header = TRUE)
-DEG.ATAC.list <- list(
-    up = c(DEG.ATAC$C_high, DEG.ATAC$NM_low) %>% setdiff(""),
-    down = c(DEG.ATAC$NM_high, DEG.ATAC$C_low) %>% setdiff("")
-)
-sapply(DEG.ATAC.list, length)
-
-table(proj_Epi$Epi_type)
+## 3.2. ATAC data ----
 marker.ATAC <- getMarkerFeatures(
     ArchRProj = proj_Epi,
     useMatrix = "GeneScoreMatrix",
@@ -224,36 +378,71 @@ marker.ATAC <- getMarkerFeatures(
     bgdGroups = "Normal"
 )
 
-marker.ATAC.df <- do.call(cbind, marker.ATAC@assays@data)
-rownames(marker.ATAC.df) <- marker.ATAC@elementMetadata$name
-colnames(marker.ATAC.df) <- names(marker.ATAC@assays@data)
+marker.ATAC.df <- getMarkers(marker.ATAC,
+    cutOff = "FDR <= 0.05 & abs(Log2FC) >= 0.25"
+)
+marker.ATAC.df <- marker.ATAC.df$Malignant %>% as.data.frame()
+dim(marker.ATAC.df)
 
-## 3.4. compare DEGs ----
+marker.ATAC.df$DEG <- "none"
+marker.ATAC.df[marker.ATAC.df$Log2FC > 0, "DEG"] <- "up"
+marker.ATAC.df[marker.ATAC.df$Log2FC < 0, "DEG"] <- "down"
+table(marker.ATAC.df$DEG)
+
+DEG.ATAC.list <- list(
+    up = marker.ATAC.df$name[marker.ATAC.df$DEG == "up"],
+    down = marker.ATAC.df$name[marker.ATAC.df$DEG == "down"]
+)
+
+## 3.3. compare DEGs ----
 genes.ATAC <- ArchR::getFeatures(proj_Epi) %>% as.character()
 genes.TCGA <- rownames(marker.TCGA)
-genes.10X <- rownames(marker.10X)
+genes.10X <- rownames(epi.obj)
 gene.common <- intersect(genes.ATAC, genes.TCGA) %>%
     intersect(genes.10X)
 
 v <- Vennerable::Venn(list(
-    "ATAC" = DEG.ATAC.list$up %>% intersect(gene.common),
+    "scATAC" = DEG.ATAC.list$up %>% intersect(gene.common),
     "TCGA" = marker.TCGA.list$up %>% intersect(gene.common),
-    "10X" = marker.10X.list$up %>% intersect(gene.common)
+    "NG_10X_scRNA" = marker.10X.list$up %>% intersect(gene.common)
 ))
 intersect(marker.TCGA.list$up, DEG.ATAC.list$up) %>% sort()
 
-pdf("Venn.ATAC.Up.pdf", 10, 10)
+pdf("Venn.ATAC.merge.Up.pdf", 10, 10)
 plot(v, doWeights = TRUE, doEuler = TRUE, show = list(Faces = FALSE))
 dev.off()
 
 v <- Vennerable::Venn(list(
-    "ATAC" = DEG.ATAC.list$down %>% intersect(gene.common),
-    "TCGA" = marker.TCGA.list$down %>% intersect(gene.common),
-    "10X" = marker.10X.list$down %>% intersect(gene.common)
+    "scATAC" = DEG.ATAC.list$up %>% intersect(gene.common),
+    "TCGA" = marker.TCGA.list$up %>% intersect(gene.common),
+    "NG_10X_scRNA" = unlist(marker.10x.patient.up) %>%
+        unique() %>%
+        intersect(gene.common)
 ))
-pdf("Venn.ATAC.Down.pdf", 10, 10)
+pdf("Venn.ATAC.patient.Up.pdf", 10, 10)
 plot(v, doWeights = TRUE, doEuler = TRUE, show = list(Faces = FALSE))
 dev.off()
+
+v <- Vennerable::Venn(list(
+    "scATAC" = DEG.ATAC.list$down %>% intersect(gene.common),
+    "TCGA" = marker.TCGA.list$down %>% intersect(gene.common),
+    "NG_10X_scRNA" = marker.10X.list$down %>% intersect(gene.common)
+))
+pdf("Venn.ATAC.merge.Down.pdf", 10, 10)
+plot(v, doWeights = TRUE, doEuler = TRUE, show = list(Faces = FALSE))
+dev.off()
+
+v <- Vennerable::Venn(list(
+    "scATAC" = DEG.ATAC.list$down %>% intersect(gene.common),
+    "TCGA" = marker.TCGA.list$down %>% intersect(gene.common),
+    "NG_10X_scRNA" = unlist(marker.10x.patient.down) %>%
+        unique() %>%
+        intersect(gene.common)
+))
+pdf("Venn.ATAC.patient.Down.pdf", 10, 10)
+plot(v, doWeights = TRUE, doEuler = TRUE, show = list(Faces = FALSE))
+dev.off()
+
 rm(p, plot.data, foo, v)
 
 rm(proj_Epi)
